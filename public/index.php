c9de05feea7132a495d74bf9fe28ef04331386b3
CommanderMark <markjarjour@gmail.com>
2020-03-14 18:48:02 -0400

    Inline CSS.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,2 @@
 <?php
-define('CSS_DIR', '');
 include(__DIR__ . '/../src/EntryPoint.php');
--- src/EntryPoint.php
+++ src/EntryPoint.php
@@ -2,4 +2,8 @@
 include(__DIR__ . '/InitializeRoutes.php');
 
 ?>
-<link rel="stylesheet" type="text/css" href="<?= CSS_DIR . '/' ?>style.css">
+<style>
+    body {
+        color: red;
+    }
+</style>
